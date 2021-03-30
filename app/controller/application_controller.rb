888e887fd71513f051e87c7b99f9f6ace6721119
jean <jean.lee.8282@gmail.com>
2021-03-29 21:48:51 -0400

    modified

--- app/controller/application_controller.rb
+++ app/controller/application_controller.rb
@@ -21,11 +21,11 @@ class ApplicationController < Sinatra::Base
     get '/result' do
         exsist=(1..415).to_a 
         @cocktail = Cocktail.find_by(name: params[:name])
-        if @cocktail.nil? && !logged_in?
-            flash[:error] = "There is no such a cocktail under that name in our website, why don't you try to create one?"
-            redirect "/login"
-        elsif !@cocktail.nil? && !logged_in? && !exsist.include?(@cocktail.id)
+        if !@cocktail.nil? && !logged_in? && !exsist.include?(@cocktail.id)
             flash[:error] = "You don't have access for this cocktail. This cocktail is belong to another user, why don't you try to create one for yourself?"
+            redirect "/login"
+        elsif @cocktail.nil? && !logged_in?
+            flash[:error] = "There is no such a cocktail under that name in our website, why don't you try to create one?"
             redirect "/login"
         elsif @cocktail==nil && logged_in?
             flash[:error] = "There is no such a cocktail under that name in our website, why don't you try to create one?"
